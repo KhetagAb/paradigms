68957042c129ca475bdae3aee18a7ec16253b942
Khetag Dzestelov <dzkhetag@gmail.com>
2021-04-08 17:40:18 +0300

    [HM-6] Adding factories

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -1,115 +1,69 @@
 "use strict";
 
-const zero = new Const(0)
-const one = new Const(1)
-
-function Const(val) {
-    const value = val
-    return {
-        evaluate:   () => value,
-        diff:       () => zero,
-        toString:   () => value.toString()
-    }
-}
-
 const varIndexes = {
     'x': 0,
     'y': 1,
     'z': 2
 }
 
-function Variable(name) {
-    const index = varIndexes[name]
-    return {
-        evaluate:   (...vars) => vars[index],
-        diff:       (variable) => varIndexes[variable] === index ? one : zero,
-        toString:   () => name
+function tokenFactory(constructor, evaluate, diff, toString) {
+    constructor.prototype = {
+        evaluate: evaluate,
+        diff: diff,
+        toString: toString,
+        constructor: constructor
     }
+
+    return constructor
 }
 
-function Operator(...operands) {
-    this.operands = operands
-}
-Operator.prototype = {
-    evaluate:   function (...vars) { return this.operate(...this.operands.map(ex => ex.evaluate(...vars))) },
-    diff:       function (variable) { return this.differ(variable)(...this.operands) },
-    toString:   function () { return this.operands.concat(this.symbol).join(" ") }
+const Const = tokenFactory(
+    function (value) { this.value = value },
+    function () { return this.value },
+    () => zero,
+    function () { return this.value.toString() })
+const Variable = tokenFactory(
+    function (variable) { this.index = varIndexes[variable]; this.value = variable;  },
+    function (...vars) { return vars[this.index] },
+    function (variable) { return this.value === variable ? one : zero },
+    function () { return this.value.toString() })
+const Operator =  tokenFactory(
+    function (...operands) { this.operands = operands },
+    function (...vars) { return this.operate(...this.operands.map(ex => ex.evaluate(...vars))) },
+    function (variable) { return this.differ(variable)(...this.operands) },
+    function () { return this.operands.concat(this.symbol).join(" ") })
+
+const one = new Const(1)
+const zero = new Const(0)
+
+function operatorFactory(symbol, operate, differ) {
+    const operator = function (...operands) {
+        Operator.call(this, ...operands)
+    }
+    operator.prototype = Object.create(Operator.prototype);
+    operator.prototype.symbol = symbol;
+    operator.prototype.operate = operate;
+    operator.prototype.differ = differ
+    operator.prototype.constructor = operator;
+    return operator;
 }
 
-function operatorPrototypeInit(constructor, symbol, operate, differ) {
-    constructor.prototype = Object.create(Operator.prototype);
-    constructor.prototype.symbol = symbol
-    constructor.prototype.operate = operate
-    constructor.prototype.differ = differ
-    constructor.prototype.constructor = constructor
-}
-
-function Add(left, right) {
-    Operator.call(this, left, right);
-}
-operatorPrototypeInit(Add,
-    "+",
-    (x, y) => x + y,
-    d => (x, y) => new Add(x.diff(d), y.diff(d)));
-
-function Subtract(left, right) {
-    Operator.call(this, left, right);
-}
-operatorPrototypeInit(Subtract,
-    "-",
-    (x, y) => x - y,
-    d => (x, y) => new Subtract(x.diff(d), y.diff(d)));
-
-function Multiply(left, right) {
-    Operator.call(this, left, right);
-}
-operatorPrototypeInit(Multiply,
-    "*",
-    (x, y) => x * y,
-    d => (x, y) => new Add(new Multiply(x.diff(d), y), new Multiply(x, y.diff(d))))
-
-function Divide(left, right) {
-    Operator.call(this, left, right);
-}
-operatorPrototypeInit(Divide,
-    "/",
-    (x, y) => x / y,
-    d => (x, y) => new Divide(new Subtract(new Multiply(x.diff(d), y), new Multiply(y.diff(d), x)), new Multiply(y, y)))
-
-function Hypot(left, right) {
-    Operator.call(this, left, right)
-}
-operatorPrototypeInit(Hypot,
-    "hypot",
-    (x, y) => x * x + y * y,
-    d => (x, y) => new Add(new Multiply(x, x), new Multiply(y, y)).diff(d)
-)
-
-function HMean(left, right) {
-    Operator.call(this, left, right)
-}4
-operatorPrototypeInit(HMean,
-    "hmean",
-    (x, y) => 2 / (1 / x + 1 / y),
-    d => (x, y) => new Divide(new Const(2), new Add(new Divide(one, x), new Divide(one, y))).diff(d)
-)
-
-function Negate(value) {
-    Operator.call(this, value);
-}
-operatorPrototypeInit(Negate,
-    "negate",
-    x => -x,
-    d => x => new Negate(x.diff(d)));
+const Add =     operatorFactory("+", (x, y) => x + y, d => (x, y) => new Add(x.diff(d), y.diff(d)));
+const Subtract = operatorFactory("-", (x, y) => x - y, d => (x, y) => new Subtract(x.diff(d), y.diff(d)));
+const Multiply = operatorFactory("*", (x, y) => x * y, d => (x, y) => new Add(new Multiply(x.diff(d), y), new Multiply(x, y.diff(d))))
+const Divide =  operatorFactory("/", (x, y) => x / y, d => (x, y) => new Divide(new Subtract(new Multiply(x.diff(d), y), new Multiply(y.diff(d), x)), new Multiply(y, y)))
+const Hypot =   operatorFactory("hypot", (x, y) => x * x + y * y, d => (x, y) => new Add(new Multiply(x, x), new Multiply(y, y)).diff(d))
+const HMean =   operatorFactory("hmean", (x, y) => 2 / (1 / x + 1 / y), d => (x, y) => new Divide(new Const(2), new Add(new Divide(one, x), new Divide(one, y))).diff(d))
+const Negate =  operatorFactory("negate", x => -x, d => x => new Negate(x.diff(d)));
 
 const operators = {
-    '+': Add,
-    '-': Subtract,
-    '*': Multiply,
-    '/': Divide,
-    'negate': Negate,
-    'hypot': Hypot,
-    'hmean': HMean
+    '+': [Add, 2],
+    '-': [Subtract, 2],
+    '*': [Multiply, 2],
+    '/': [Divide, 2],
+    'negate': [Negate, 1],
+    'hypot': [Hypot, 2],
+    'hmean': [HMean, 2]
 }
 
 const parse = input => {
@@ -117,7 +71,7 @@ const parse = input => {
         let top
         if (token in operators) {
             let operator = operators[token]
-            top = new operator(...stack.splice(stack.length - operator.length))
+            top = new operator[0](...stack.splice(stack.length - operator[1]))
         } else if (token in varIndexes) {
             top = new Variable(token)
         } else {
