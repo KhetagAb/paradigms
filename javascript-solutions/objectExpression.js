9de29457ba5f56a6bf8bc740bf96a431d4935ca0
Khetag Dzestelov <dzkhetag@gmail.com>
2021-04-16 17:33:40 +0300

    [HM-6] Fixing solution

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -11,6 +11,7 @@ const AbstractExpression = {
         evaluate: function (...vars) { return this.operate(...this.operands.map(ex => ex.evaluate(...vars))) },
         diff: function (variable) { return this.differ(variable, ...this.operands, ...(this.operands.map(e => e.diff(variable)))) },
         prefix() { return this.toString(); },
+        postfix() { return this.toString(); },
         toString: function () { return this.operands[0].toString() }
     },
     init: (...params) => {
@@ -37,6 +38,7 @@ function abstractOperation(symbol, operateFun, differFun) {
         function operate(...args) { return operateFun(...args); },
         function differ(variable, ...args) { return differFun(variable, ...args); },
         function prefix() { return "(" + [].concat(symbol).concat(this.operands.map(e => e.prefix())).join(" ") + ")" },
+        function postfix() { return "(" + [].concat(this.operands.map(e => e.postfix())).concat(symbol).join(" ") + ")"},
         function toString() { return this.operands.concat(symbol).join(" "); })
 }
 
@@ -47,6 +49,9 @@ const Divide =   abstractOperation("/", (x, y) => x / y, (d, x, y) => new Divide
 const Hypot =    abstractOperation("hypot", (x, y) => x * x + y * y, (d, x, y) => new Add(new Multiply(x, x), new Multiply(y, y)).diff(d))
 const HMean =    abstractOperation("hmean", (x, y) => 2 / (1 / x + 1 / y), (d, x, y) => new Divide(new Const(2), new Add(new Divide(Const.one, x), new Divide(Const.one, y))).diff(d))
 const Negate =   abstractOperation("negate", x => -x, (d, x) => new Negate(x.diff(d)));
+const ArithMean = abstractOperation("arith-mean", (...args) => args.reduce((sum, summoned) => sum + summoned, 0) / args.length, );
+const GeomMean = abstractOperation("geom-mean", (...args) => Math.pow(Math.abs(args.reduce((prod, multiplier) => prod * multiplier, 1)), 1 / args.length), ); // toDo
+const HarmMean = abstractOperation("harm-mean", (...args) => args.length / args.reduce((sum, summoned) => sum + 1 / summoned, 0), );
 
 const operators = {
     '+': [Add, 2],
@@ -55,7 +60,10 @@ const operators = {
     '/': [Divide, 2],
     'negate': [Negate, 1],
     'hypot': [Hypot, 2],
-    'hmean': [HMean, 2]
+    'hmean': [HMean, 2],
+    'arith-mean': [ArithMean, Infinity],
+    'geom-mean': [GeomMean, Infinity],
+    'harm-mean': [HarmMean, Infinity]
 }
 
 const parse = input => {
@@ -73,45 +81,101 @@ const parse = input => {
     }, []).pop();
 }
 
-// (- (* 2 x) 3)
-// (+ ...)
-
-// ex = @ <ex> <ex> <ex> <ex>
-// ex = <ex> <ex> <ex> @
-// ex = 1
-// ex = x
+// ex = (@ <ex> <ex> <ex> ...)
+// ex = (<ex> <ex> <ex> ... @)
+// ex = const
+// ex = variable
 function parser(input) {
     let source = {
+        EOF: 0,
         index: 0,
-        input: input.replace(/[(]/g, " ( ").replace(/[)]/g, " ) ").split(" ").filter(e => e !== "");
-        hasNext: index < this.input.length,
-        next: this.input[++index]
+        input: input.replace(/[(]/g, " ( ").replace(/[)]/g, " ) ").split(" ").filter(e => e !== ""),
+        hasNext: function () { return this.index < this.input.length },
+        current: function () { return this.input[this.index] },
+        next: function () { return this.hasNext() ? this.input[this.index++] : this.EOF },
+        test: function (expected) {
+            if (expected === this.current()) {
+                this.next();
+                return true;
+            } else {
+                return false;
+            }
+        },
+        expect: function (expected) {
+            if (expected === this.current()) {
+                this.next();
+            } else {
+                throw new Error("mismatch exception ");
+            }
+        }
     }
 
     return {
-        parse: function (mode) {
-            if (Number.isFinite(input[index])) {
-                return new Const(input[index])
+        parseExpression: function (mode) {
+            let parsed
+            if (source.test('(')) {
+                parsed = this.parse(mode);
+                source.expect(')');
+            } else {
+                parsed = this.parseOperands()[0];
             }
 
+            if (source.hasNext()) {
+                throw new Error("Unexpected symbols"); // toDo
+            }
+
+            return parsed;
+        },
+        parse: function (mode) {
+            let operator
+            let operands
             if (mode === "prefix") {
-                return [].concat(this.parseOperator()).concat(this.parseOperands);
+                operator = this.parseOperator(mode);
+                operands = this.parseOperands(mode);
+            } else if (mode === "postfix") {
+                operands = this.parseOperands(mode);
+                operator = this.parseOperator(mode);
             } else {
-                return [].concat(this.parseOperands()).concat(this.parseOperator());
+                throw new Error("Illegal mode"); // toDo
+            }
+
+            if (operator[1] !== Infinity && operator[1] !== operands.length) {
+                throw new Error("operands mismatch exception"); // toDO
+            } else {
+                return new operator[0](...operands);
             }
         },
-        parseOperator: () => 1,
-        parseOperands: () => 1
+        parseOperator: () => {
+            if (source.current() in operators) {
+                return operators[source.next()]
+            } else {
+                throw new Error("no such operator") // TODO;
+            }
+        },
+        parseOperands: function (mode) {
+            let operands = []
+            while (true) {
+                if (isFinite(source.current())) {
+                    operands.push(new Const(Number(source.next())));
+                } else if (source.current() in varIndexes) {
+                    operands.push(new Variable(source.next()));
+                } else if (source.test('(')) {
+                    operands.push(this.parse(mode));
+                    source.expect(')');
+                } else {
+                    break;
+                }
+            }
+
+            return operands
+        }
     }
 }
+
 function parsePrefix(input) {
-    return parser("prefix").parse(input);
+    return parser(input).parseExpression("prefix");
 }
 
-
-let ex = new Subtract(new Multiply(new Const(2), new Variable('x')), new Const(3));
-
-// console.log(ex.toString())
-// console.log(ex.prefix())
-
-console.log(parsePrefix("(-    (*  2   x)  3)"))
+function parsePostfix(input) {
+    return parser(input).parseExpression("postfix");
+}
