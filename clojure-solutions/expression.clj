4f15c2af11204fe4cb1ed9b5a6fe634cc74c8e3c
Khetag Dzestelov <dzkhetag@gmail.com>
2021-04-30 15:44:41 +0300

    [HM-9] Addmodification

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -7,18 +7,23 @@
 (defn _div
   ([f] (/ 1 (double f)))
   ([f & args] (reduce #(/ %1 (double %2)) f args)))
+(defn square [a] (* a a))
+(defn mean-op [& args] (_div (apply + args) (count args)))
+(defn varn-op [& args] (- (apply mean-op (map square args)) (square (apply mean-op args))))
 
 (def add (operator +))
 (def subtract (operator -))
 (def multiply (operator *))
 (def divide (operator _div))
+(def mean (operator mean-op))
+(def varn (operator varn-op))
 (def negate subtract)
 
-(def operators {'+ add '- subtract '* multiply '/ divide 'negate subtract})
+(def operators {'+ add '- subtract '* multiply '/ divide 'negate subtract 'mean mean 'varn varn})
 
 (defn parseFunction [input]
-  (letfn [(parse-expression [exps] (cond
-                                     (list? exps) (apply (operators (first exps)) (mapv parse-expression (rest exps)))
-                                     (number? exps) (constant exps)
-                                     :else (variable (str exps))))]
-    (parse-expression (read-string input))))
+  (letfn [(parse-expr [exps] (cond
+                               (list? exps) (apply (operators (first exps)) (mapv parse-expr (rest exps)))
+                               (number? exps) (constant exps)
+                               :else (variable (str exps))))]
+    (parse-expr (read-string input))))
